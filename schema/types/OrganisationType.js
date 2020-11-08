72d23a17e2393c4ed4cf0a3c37de6868d571238f
Yegor Trussov <egortr2004@mail.ru>
2020-11-08 10:01:28 +0600

    type relations between user and organisation types added

--- schema/types/OrganisationType.js
+++ schema/types/OrganisationType.js
@@ -1,19 +1,30 @@
 const graphql = require('graphql');
 
+const User = require('../../models/User');
+
 const { GraphQLObjectType, 
         GraphQLID, 
         GraphQLInt, 
-        GraphQLList,
         GraphQLString,
-        GraphQLBoolean } = graphql;
+        GraphQLBoolean,
+        GraphQLList } = graphql;
+
 
 const OrganisationType = new GraphQLObjectType({
     name: 'Organisation',
     fields: () =>  ({
         _id: { type: GraphQLID },
-        name: { type: GraphQLString }
-        // featuredContests: {  }
+        name: { type: GraphQLString },
+        members: {
+            type: new GraphQLList(UserType),
+            resolve(parent, args) {
+                return User 
+                        .find({ _id: { $in: parent.members } })
+            }
+        }
     })
 })
 
 module.exports = OrganisationType;
+
+const UserType = require('./UserType');
--- schema/types/UserType.js
+++ schema/types/UserType.js
@@ -1,5 +1,7 @@
 const graphql = require('graphql');
 
+const Organisation = require('../../models/Organisation');
+
 const { GraphQLObjectType, 
         GraphQLID, 
         GraphQLInt, 
@@ -14,12 +16,19 @@ const UserType = new GraphQLObjectType({
     fields: () =>  ({
         _id: { type: GraphQLID },
         fullName: { type: GraphQLString },
-        organisation: { type: GraphQLString },
         isTeacher: { type: GraphQLBoolean },
         isAdmin: { type: GraphQLBoolean },
-        grade: { type: GraphQLInt }
-        // featuredContests: {  }
+        grade: { type: GraphQLInt },
+        organisation: {
+            type: OrganisationType,
+            resolve(parent, args) {
+                return Organisation 
+                    .findOne({ _id: parent.organisation });
+            }
+        }
     })
 })
 
 module.exports = UserType;
+
+const OrganisationType = require('./OrganisationType');
