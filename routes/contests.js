d17a9c7c452e2d38547ea068c3d9e9dadec85c63
Yegor Trussov <egortr2004@mail.ru>
2020-11-09 15:10:59 +0600

    feature contest functionality added

--- routes/contests.js
+++ routes/contests.js
@@ -4,7 +4,7 @@ const Contest = require('../models/Contest');
 const User = require('../models/User');
 const Subject = require('../models/Subject');
 
-const { teacherAuth } = require('../middleware/auth');
+const { teacherAuth, userAuth } = require('../middleware/auth');
 
 const router = express.Router();
 
@@ -69,6 +69,32 @@ router.post('/deleteContest', teacherAuth, (req, res) => {
                         .status(200)
                         .json({
                             success: true
+                        })
+                })
+        })
+})
+
+router.post('/featureContest', userAuth, (req, res) => {
+    const { userId } = req.user;
+    const { contestId } = req.body;
+
+    User 
+        .findOne({ _id: userId })
+        .then(foundUser => {
+            if (foundUser.featuredContests.indexOf(contestId) >= 0) {
+                foundUser.featuredContests = foundUser.featuredContests.filter(contest => contest !== contestId);
+            } else {
+                foundUser.featuredContests.push(contestId);
+            }
+
+            foundUser 
+                .save()
+                .then(savedUser => {
+                    res 
+                        .status(200)
+                        .json({
+                            success: true,
+                            user: savedUser
                         })
                 })
         })
